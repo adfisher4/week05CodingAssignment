fc106231c4fff12e2add6e3665040c302a26b7af
Adam Fisher <adfisher4@gmail.com>
2024-09-07 12:57:15 -0500

    second commit

--- App.java
+++ App.java
@@ -5,11 +5,12 @@ public class App {
 
 	public static void main(String[] args) {
 		
+		// instance of each class from Logger interface
 		Logger astGreet = new AsteriskLogger("Can Hello be an error?");
 		Logger spaceGreet = new SpacedLogger("Why is Hello so wrong!?");
 		
 		
-		
+		// Test AsteriskLogger methods
 		astGreet.log("Hello");
 		System.out.println();
 		astGreet.error("Errorness hello. Huh?");
@@ -18,6 +19,7 @@ public class App {
 		System.out.println("Checking SpacedLogger methods:");
 		System.out.println();
 		
+		// Tests SpacedLogger methods
 		spaceGreet.log("Well HELLO there!");
 		System.out.println();
 		spaceGreet.error("Why is Hello so wrong!");
